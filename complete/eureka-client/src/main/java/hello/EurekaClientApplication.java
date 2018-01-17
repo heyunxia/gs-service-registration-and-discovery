8afb7671033f841c0ee24102e2f87573bf58416f
He Yunxia <love3400wind@163.com>
2018-01-17 18:42:39 +0800

    修复未引入包
    修复未引入包  @Value

--- complete/eureka-client/src/main/java/hello/EurekaClientApplication.java
+++ complete/eureka-client/src/main/java/hello/EurekaClientApplication.java
@@ -1,6 +1,7 @@
 package hello;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.ServiceInstance;
